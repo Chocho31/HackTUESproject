84d1a92689a06b7c7ea05cd441b21adb815e8a80
Tsenko Tsenkov <tseni.tsenkov@gmail.com>
2018-03-17 15:14:50 +0200

    added upgrade menu

---
+++ UbgradeMenu/Assets/PopUpScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpScript : MonoBehaviour {
+	public GameObject image;
+	void OnMouseOver(){
+		Debug.Log ("Hello");
+		image.SetActive(true);
+	}
+	void OnMouseExit(){
+		image.SetActive (false);
+	}
+}
