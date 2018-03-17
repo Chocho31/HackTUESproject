6086d1061015f8aaa167f75a5eb1183b5796fe22
Chocho31 <chudomir.miroslavov.chenkov@gmail.com>
2018-03-17 08:31:34 +0200

    giving cecou his menu back

---
+++ InventoryMenu/Assets/InventoryMenuScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InventoryMenuScript : MonoBehaviour {
+
+	public void Back_Button() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex - 1);
+	}
+	public void Equip_Button() {
+		return;
+	}
+}
