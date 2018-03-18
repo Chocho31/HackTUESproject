28500991fad8e13b5f33f06c597ed5c74efa80fb
Deqn <deqnberberov@abv.bg>
2018-03-18 08:51:09 +0200

    Final game

---
+++ Final Product/Game/Assets/Scripts/Laser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    
+    GameObject HM;
+    HeartManagment heartManager;
+
+
+
+
+    void Awake()
+    {
+  
+
+        HM = GameObject.FindGameObjectWithTag("MainCamera");
+        heartManager = HM.GetComponent<HeartManagment>();
+    }
+
+
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="colliderForBullets")
+        {
+            heartManager.Hide();
+        }
+    }
+}
+
